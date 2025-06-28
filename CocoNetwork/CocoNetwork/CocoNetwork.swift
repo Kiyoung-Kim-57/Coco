b85f5b0e3a42e8f92f9e471de0131058050e76f9
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-06-28 21:52:00 +0900

    네트워크 모듈 추가

---
+++ CocoNetwork/CocoNetwork/CocoNetwork.swift
@@ -0,0 +1,9 @@
+//
+//  CocoNetwork.swift
+//  CocoNetwork
+//
+//  Created by 김기영 on 6/28/25.
+//
+
+import Foundation
+
---
+++ CocoNetwork/CocoNetwork/Error/NetworkError.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkError.swift
+//  CocoNetwork
+//
+//  Created by 김기영 on 6/28/25.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case requestObjectError
+    
+    public var description: String {
+        switch self {
+        case .requestObjectError: "Failed to initialize HttpRequest"
+        }
+    }
+}
