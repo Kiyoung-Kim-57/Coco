a48dae7e347def396627c3c5724dea52d2fe3ffc
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-06-30 14:31:00 +0900

    CocoDatasource 프로젝트 추가

---
+++ CocoDatasource/CocoDatasource/CocoDatasource.swift
@@ -0,0 +1,9 @@
+//
+//  CocoDatasource.swift
+//  CocoDatasource
+//
+//  Created by 김기영 on 6/30/25.
+//
+
+import Foundation
+
---
+++ CocoDatasource/CocoDatasource/Interface/CocoDataSource.swift
@@ -0,0 +1,40 @@
+//
+//  PLDataSource.swift
+//  PLData
+//
+//  Created by 김기영 on 1/29/25.
+//
+
+import Foundation
+
+public protocol CocoDataSource {
+    associatedtype Item
+    associatedtype Condition
+}
+
+public protocol CocoReadableDataSource: CocoDataSource {
+    func readData(requestHandler: (Condition) -> (Condition)) async throws -> Item
+    
+    func readData<T: Decodable>(
+        type: T.Type,
+        requestHandler: (Condition) -> (Condition)
+    ) async throws -> T
+}
+
+public protocol CocoWritableDataSource: CocoDataSource {
+    // MARK: 쓰기 성공 여부를 Bool값으로 return, 이미 있다면 update 없다면 create
+    @discardableResult
+    func writeData(
+        _ item: Item,
+        requestHandler: (Condition) -> (Condition)
+    ) async throws -> Bool
+}
+
+public protocol CocoDeletableDataSource: CocoDataSource {
+    @discardableResult
+    func deleteData(
+        requestHandler: (Condition) -> (Condition)
+    ) async throws -> Bool
+}
+
+public typealias CocoDataSourceProtocol = CocoReadableDataSource & CocoWritableDataSource & CocoDeletableDataSource
