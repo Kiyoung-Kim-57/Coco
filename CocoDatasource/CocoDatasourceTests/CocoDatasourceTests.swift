0e487f2003bd508386b0b409bbd00e4ee079706f
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-07-25 15:51:57 +0900

    데이터소스 테스트 파일에 테스트용 샘플 로직 생성

--- CocoDatasource/CocoDatasourceTests/CocoDatasourceTests.swift
+++ CocoDatasource/CocoDatasourceTests/CocoDatasourceTests.swift
@@ -6,11 +6,35 @@
 //
 
 import Testing
+import CocoNetwork
+import CocoDatasource
 
+@Suite("Remote Datasource Test")
 struct CocoDatasourceTests {
+    private var networkManager: NetworkManager
+    private var remoteDatasource: CocoRemoteDataSource
+    
+    init() {
+        self.networkManager = NetworkManagerImpl()
+        self.remoteDatasource = CocoRemoteDataSource(networkManager: self.networkManager)
+    }
+    
+    @Test("Read Data Test")
+    func readData() async throws {
+        await #expect(throws: TestErrors.readDataFailed) {
+            let data = try await remoteDatasource.readData { request in
+                request
+                    .setURLPath(path: "")
+                    .addHeader(key: "", value: "")
+                    .addQueryItem("", "")
+                    .changeQueryItemValue("", "")
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+            }
+        }
     }
 
+}
+
+enum TestErrors: Error {
+    case readDataFailed
 }
