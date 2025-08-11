ea1da849d219568f46956e67af09c7da3f138fd6
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-08-11 14:15:10 +0900

    코인 리스트 DTO 추가

---
+++ CocoDatasource/CocoDatasource/DTO/MarketEvent.swift
@@ -0,0 +1,14 @@
+//
+//  MarketEvent.swift
+//  CocoDatasource
+//
+//  Created by 김기영 on 8/11/25.
+//
+
+
+// MARK: - Market Event Structure
+public struct MarketEvent: Codable {
+    let warning: Bool
+    let caution: Caution
+}
+
