d4cb33e32f8f731b920fee87f3b5fb20740c3be6
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-08-12 15:55:36 +0900

    Responsable 프로토콜에 메서드 추가

--- CocoNetwork/CocoNetwork/HTTP/Interface/Responsable.swift
+++ CocoNetwork/CocoNetwork/HTTP/Interface/Responsable.swift
@@ -5,4 +5,13 @@ public protocol Responsable {
     
     var statusCode: Int { get set }
     var response: ResponseType { get set }
+    
+    @discardableResult
+    func onStatus(_ statusCode: Int, handler: @escaping (Self) -> Void) -> Self
+    
+    @discardableResult
+    func onStatus(in range: ClosedRange<Int>, handler: @escaping (Self) -> Void) -> Self
+    
+    @discardableResult
+    func onStatus(oneOf statusCodes: [Int], handler: @escaping (Self) -> Void) -> Self
 }
