af23cd06d02e7046cab681b5bbc304449e63ce54
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-08-15 15:43:37 +0900

    커스텀 레이블 뷰 생성

---
+++ Coco/CocoDesign/BaseComponent/CocoLabel.swift
@@ -0,0 +1,45 @@
+//
+//  BaseLabel.swift
+//  CocoDesign
+//
+//  Created by 김기영 on 8/15/25.
+//
+
+import SwiftUI
+
+struct CocoLabel: View {
+    private let text: String
+    private let font: Font?
+    private let textColor: Color
+    private let backgroundColor: Color
+    private let textPadding: CGFloat = 10
+    private let cornerRadius: CGFloat
+    
+    init(
+        _ text: String,
+        font: Font? = nil,
+        textColor: Color = .black,
+        backgroundColor: Color = .clear,
+        textPadding: CGFloat = 10,
+        cornerRadius: CGFloat = 0
+    ) {
+        self.text = text
+        self.font = font
+        self.textColor = textColor
+        self.backgroundColor = backgroundColor
+        self.cornerRadius = cornerRadius
+    }
+    
+    var body: some View {
+        Text(text)
+            .font(font ?? .body)
+            .padding(textPadding)
+            .foregroundStyle(textColor)
+            .background(backgroundColor)
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+    }
+}
+
+#Preview {
+    CocoLabel("Hello World", backgroundColor: .gray, cornerRadius: 5)
+}
