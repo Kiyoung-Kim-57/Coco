ea3ef355d07e525f82b4b88e5377483f56222b3c
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-08-23 23:14:54 +0900

    코인 리스트 레포지토리 구현체 구현

---
+++ CocoDatasource/CocoDatasource/RepositoryImpl/CoinListRepositoryImpl.swift
@@ -0,0 +1,32 @@
+//
+//  CoinListRepositoryImpl.swift
+//  CocoDatasource
+//
+//  Created by 김기영 on 8/23/25.
+//
+
+import Foundation
+import CocoDomain
+import CocoNetwork
+
+public final class CoinListRepositoryImpl: CoinListRepository {
+    private let remoteDataSource: any CocoRemoteDataSource
+    
+    public init(remoteDataSource: any CocoRemoteDataSource) {
+        self.remoteDataSource = remoteDataSource
+    }
+    
+    public func fetchCoinList() async throws -> CoinListEntities {
+        let response = try await remoteDataSource.readData(type: CryptocurrencyMarkets.self) { request in
+            request
+                .setURLPath(path: "/v1/market/all")
+        }
+        
+        return response.map { $0.toCoinListEntity() }
+    }
+    
+    public func fetchCoinThumbnail() async throws -> URL {
+        return URL(filePath: "")
+    }
+    
+}
