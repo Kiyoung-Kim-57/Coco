ec660d4409319928c65dd6dc9024151943b08592
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-08-27 00:13:27 +0900

    업비트api 요청용 네임스페이스 생성

---
+++ CocoDatasource/CocoDatasource/Enum/Upbit.swift
@@ -0,0 +1,12 @@
+//
+//  Upbit.swift
+//  CocoDatasource
+//
+//  Created by 김기영 on 8/26/25.
+//
+
+public enum Upbit {
+    public static func marketPath() -> String {
+        return "/v1/market/all"
+    }
+}
