8d6ebc0190a24e3c0081049e49e6054ace449512
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-08-29 22:07:47 +0900

    트렌드 코인 엔티티 SVG 부분 수정
    - Url string에서 data 타입으로 변경

--- CocoDomain/CocoDomain/Entity/TrendingCoinListEntity.swift
+++ CocoDomain/CocoDomain/Entity/TrendingCoinListEntity.swift
@@ -4,6 +4,7 @@
 //
 //  Created by 김기영 on 8/26/25.
 //
+import Foundation
 
 public struct TrendingCoinListEntity: Comparable, Hashable {
     public let code: String
@@ -12,16 +13,16 @@ public struct TrendingCoinListEntity: Comparable, Hashable {
     public let changeRate: Double
     public let thumbUrl: String?
     public let rank: Int
-    public let sparklineUrl: String?
+    public let sparkline: Data
     
-    public init(code: String, name: String, price: Double, changeRate: Double, thumbUrl: String?, rank: Int, sparklineUrl: String?) {
+    public init(code: String, name: String, price: Double, changeRate: Double, thumbUrl: String?, rank: Int, sparkline: Data) {
         self.code = code
         self.name = name
         self.price = price
         self.changeRate = changeRate
         self.thumbUrl = thumbUrl
         self.rank = rank
-        self.sparklineUrl = sparklineUrl
+        self.sparkline = sparkline
     }
     // Comparable
     public static func < (lhs: TrendingCoinListEntity, rhs: TrendingCoinListEntity) -> Bool {
