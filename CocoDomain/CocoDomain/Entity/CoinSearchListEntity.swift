eb021b71544886400f2c54a5e8e7f503d065b838
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-09-05 13:21:02 +0900

    CoinSearchListEntity 구현

---
+++ CocoDomain/CocoDomain/Entity/CoinSearchListEntity.swift
@@ -0,0 +1,22 @@
+//
+//  CoinSearchListEntity.swift
+//  CocoDomain
+//
+//  Created by 김기영 on 9/5/25.
+//
+
+import Foundation
+
+public struct CoinSearchListEntity {
+    public let id: String
+    public let name: String
+    public let symbol: String
+    
+    public init(id: String, name: String, symbol: String) {
+        self.id = id
+        self.name = name
+        self.symbol = symbol
+    }
+}
+
+public typealias CoinSearchListEntities = [CoinSearchListEntity]
